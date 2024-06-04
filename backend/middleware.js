a9df5cf1cf7aa7a006acc94b515f23b790f2d2f6
Ayushmaan Pandey <ayushmaan1122pandey@gmail.com>
2024-06-05 00:31:12 +0530

    userRouter definition completed

---
+++ backend/middleware.js
@@ -0,0 +1,29 @@
+import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken";
+
+const authMiddleware = (req,res,next)=> {
+    const authHeader = req.headers.authorization;
+    if(!authHeader || !authHeader.startsWith('Bearer '))
+    {
+        return res.status(403).json({
+            message: "Token is not present"
+        });
+    }
+    const token = authHeader.split(' ')[1];
+    try
+    {
+        const decoded = jwt.verify(token,JWT_SECRET);
+        if(decoded.userId)
+        {
+            req.userId = decoded.userId;
+            next();   
+        }
+    }
+    catch(err)
+    {
+        return res.status(403).json({
+            message: "Unauthorized request"
+        });
+    }
+}
+export {authMiddleware};
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -3,6 +3,7 @@ import zod from "zod";
 import { User } from "../db";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "../config";
+import { authMiddleware } from "../middleware";
 const userRouter = Router();
 
 
@@ -40,7 +41,88 @@ userRouter.post("/signup", async (req,res)=> {
         token: token
     });
 });
- 
 
 
-export {userRouter}; 
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+});
+
+userRouter.post("/singin", async (req,res)=> {
+    const {success} = signinBody.safeParse(req.body);
+    if(!success)
+    {
+        return res.json({
+            message: "Invalid User input for signing in"
+        })    
+    }
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+    if(user){
+        const token = jwt.sign({
+            userId: user._id
+        },JWT_SECRET);
+        res.json({
+            token:token
+        })
+        return ;
+    }
+
+    return res.status(411).json({
+        message: "Error while loggin in"
+    });
+})
+
+const updateBody = zod.object({
+    password: zod.string().optional(),
+    firstName: zod.string().optional(),
+    lastName: zod.string().optional()
+});
+
+userRouter.put("/",authMiddleware, async (req,res) => {
+    const {success} = updateBody.safeParse(req.body);
+    if(!success)
+    {
+        return res.status(403).json({
+            message: "Error while updating the information"
+        });   
+    }
+    const updatedUser = await  User.updateOne(req.body,{
+        _id : req.userId
+    });
+    if(updatedUser)
+    {
+        return res.status(200).json({
+            message: "updated succesfully"
+        });    
+    }
+});
+
+userRouter.get("/bulk",async(req,res)=> {
+    const filter = req.query.filter || "";
+    const users = await User.find({
+        $or: [{
+            firstName: {
+                "$regex" : filter
+            }
+        },{
+            lastName: {
+                "$regex" : filter
+            }
+        }]
+    });
+    res.json({
+        message: "Fetched successfully",
+        user: users.map((user)=> ({
+            username: user.username,
+            firstName: user.firstName,
+            lastName: user.lastName,
+            _id: user._id
+        }))
+    })
+})
+
+
+export {userRouter} ; 
